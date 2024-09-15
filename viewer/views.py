1d22b79e7afb84a29401936ec71107299db6d47b
Petr Mrazek <petr.mrazek.jr@gmail.com>
2024-09-15 13:00:23 +0200

    docs: sekce tipy

--- viewer/views.py
+++ viewer/views.py
@@ -1,6 +1,10 @@
 from django.views.generic import TemplateView
+from viewer.models import Product
 
 class HomePageView(TemplateView):
     template_name = 'main.html'
-    extra_context = {}
+    extra_context = {
+        'all_products': Product.objects.all(),
+
+    }
 
