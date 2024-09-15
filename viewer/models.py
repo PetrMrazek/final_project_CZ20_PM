5b1d5c20e359c1b015e49366c9bc656efaf6b5df
Petr Mrazek <petr.mrazek.jr@gmail.com>
2024-09-15 13:41:17 +0200

    docs: posun prubezneho stavu 2

--- viewer/models.py
+++ viewer/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.db.models import (
+    DO_NOTHING, CharField, DateField, DateTimeField, ForeignKey, IntegerField,
+    Model, TextField
+)
 
 # Create your models here.
 
 class Product(models.Model):
     name = CharField(max_length=128)
+    weight = IntegerField()
