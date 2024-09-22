1d2bdc56ec95d5aee06c663f2c4ba06a80708dc9
AlbertA <albert.mysak@gmail.com>
2024-09-22 11:12:55 +0200

    feat: many to many

--- viewer/models.py
+++ viewer/models.py
@@ -6,15 +6,15 @@ from django.db.models import (CharField, DateField, DateTimeField, ForeignKey, I
 class Categorie(models.Model):
     name = models.CharField(max_length=255)
 
+
+class Allergen(models.Model):
+    name = models.CharField(max_length=255)
+    allergen_number = models.IntegerField()
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     thumbnail = models.ImageField(upload_to='products/', null=True, blank=True)
     category = models.ForeignKey(Categorie, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    allergens = models.ForeignKey('Allergen', on_delete=models.CASCADE)
-
-class Allergen(models.Model):
-    name = models.CharField(max_length=255)
-    allergen_number = models.IntegerField()
-
+    allergens = models.ManyToManyField(Allergen)
